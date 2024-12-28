66593cadeaf208261cf6de93807b56fd76da3eac
Dobrobatko Dmytro <dobrobatka@gmail.com>
2024-12-28 19:52:05 +0200

    fix images paths

--- src/components/MovieCastItem/MovieCastItem.tsx
+++ src/components/MovieCastItem/MovieCastItem.tsx
@@ -6,10 +6,7 @@ const MovieCastItem: React.FC<IMovieCastItemProps> = ({ name, photo, character }
   return (
     <>
       <div className={css['cast-image-wrapper']}>
-        <img
-          src={photo ? 'https://image.tmdb.org/t/p/w200' + photo : '../../public/image.png'}
-          alt={name}
-        />
+        <img src={photo ? 'https://image.tmdb.org/t/p/w200' + photo : '/image.png'} alt={name} />
       </div>
       <p className={css['cast-name']}>{name}</p>
       <p className={css['cast-character']}>Character: {character}</p>
